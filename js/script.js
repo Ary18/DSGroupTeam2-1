bd720362c24693ebefdd5b942f1a06b765bd1bbe
Francesco Anania <fra.anania94@gmail.com>
2018-03-01 17:46:01 +0100

    Progress su issueBootstrap

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
 /* globals $ */
 
 window.addEventListener('load', initializePage);
-$('body').loading();
+//$('body').loading();
 moment.locale('it');
 
 function initializePage() {
@@ -58,16 +58,16 @@ function funzioneOk(position) {
         oggetto.generaOggetto(position, function(weatherObj) {
             document.getElementById('posizione').innerText = weatherObj.name;
             document.getElementById('weather-img').setAttribute('src', 'https://openweathermap.org/img/w/' + weatherObj.weather[0].icon + '.png');
-            document.getElementById('temp').innerText = weatherObj.main.temp+' °';
-            document.getElementById('wind-deg').innerText = ( weatherObj.wind.deg || '0' )+' °';
-            document.getElementById('wind-speed').innerText = weatherObj.wind.speed+' m/s';
-            document.getElementById('pressure').innerText = weatherObj.main.pressure+' hpa';
-            document.getElementById('humidity').innerText = weatherObj.main.humidity+' %';
+            //document.getElementById('temp').innerText = weatherObj.main.temp+' °';
+            //document.getElementById('wind-deg').innerText = ( weatherObj.wind.deg || '0' )+' °';
+            //document.getElementById('wind-speed').innerText = weatherObj.wind.speed+' m/s';
+            //document.getElementById('pressure').innerText = weatherObj.main.pressure+' hpa';
+            //document.getElementById('humidity').innerText = weatherObj.main.humidity+' %';
             document.getElementById('cloudiness').innerText = weatherObj.weather[0].description;
-            document.getElementById('sunrise').innerText = moment.unix(weatherObj.sys.sunrise).format('HH:MM');
-            document.getElementById('sunset').innerText = moment.unix(weatherObj.sys.sunset).format('HH:MM');
-            document.getElementById('long').innerText = Math.round(weatherObj.coord.lon * 100) / 100;
-            document.getElementById('lat').innerText = Math.round(weatherObj.coord.lat * 100) / 100;
+            //document.getElementById('sunrise').innerText = moment.unix(weatherObj.sys.sunrise).format('HH:MM');
+            //document.getElementById('sunset').innerText = moment.unix(weatherObj.sys.sunset).format('HH:MM');
+            //document.getElementById('long').innerText = Math.round(weatherObj.coord.lon * 100) / 100;
+            //document.getElementById('lat').innerText = Math.round(weatherObj.coord.lat * 100)   / 100;
 
             $('body').loading('stop');
         });
